238352c333f811a453739540a329b33d585de894
Fem Adisa <femiwonderful@gmail.com>
2020-11-04 18:30:10 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,7 +7,11 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
+def introduction_with_language
+  puts "Hi, my name is #{name} and I am lerning to program in #{language}."
+end
 
+def 
 
 
 
